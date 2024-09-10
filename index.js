c37d2c9560f6a06b0fc9d5588b30f5e3a9aac68d
Safwan-Mohammed <safwanhanif19@gmail.com>
2024-09-10 15:45:03 +0530

    Jenkins file updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = 8000;
 const app = express();
 
 app.use(cors({
-    origin:['http://localhost:5173'],
+    origin:['http://localhost:5173', 'https://creative-begonia-fc727d.netlify.app/'],
     credentials: true,
     methods: "GET, POST, PUT, DELETE"
 }))
