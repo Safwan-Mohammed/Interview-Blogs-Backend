4a45658595c1afbc9e44be62617e3b0ec92ed1a7
Safwan-Mohammed <safwanhanif19@gmail.com>
2024-09-10 16:12:26 +0530

    Jenkins file updated

--- config/database.config.js
+++ config/database.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async() => {
     try {
-        await mongoose.connect(process.env.MONGOOSE_URL)
+        await mongoose.connect("mongodb+srv://safwan:safwan@cluster0.4uiey.mongodb.net/Blogs/")
         console.log("Database Connection Esatblished ");
     }catch(error){
         console.log("Index file DB error ", error);
