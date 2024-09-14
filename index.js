bea3caf8b57dd47903f5b4bf3698e415634acc34
Safwan-Mohammed <safwanhanif19@gmail.com>
2024-09-14 23:17:56 +0530

    Cors Updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = 8000;
 const app = express();
 
 app.use(cors({
-    origin:['http://localhost:5173', 'https://creative-begonia-fc727d.netlify.app/'],
+    origin:['http://localhost:3000', 'https://safwanspehere.online'],
     credentials: true,
     methods: "GET, POST, PUT, DELETE"
 }))
